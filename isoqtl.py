26e06b94dfc145cfe55264d6fdf1a2f0239d1b77
Nathan LaPierre <nathanl2012@gmail.com>
2021-07-22 13:02:50 -0400

    put main pass in cython and optimized

--- isoqtl.py
+++ isoqtl.py
@@ -11,6 +11,8 @@ from scipy.stats import linregress, beta
 from scipy.optimize import minimize
 # import statsmodels.api as sm
 import statsmodels.formula.api as smf
+from isoqtl_cis_pass import nominal_pass
+#from hashimoto_ohtani_cython import hashimoto_ohtani_t2_stat
 
 
 def parseargs():    # handle user arguments
@@ -108,172 +110,6 @@ def check_vcf(vcf, tx2expr):
     return meta_lines
 
 
-def preprocess_snp2geno(tx2expr, snp2geno):
-    # remove genotypes for non-phenotyped individuals and remove "nan" entries
-    snp2geno = (snp2geno.T[list(tx2expr.index)]).T  # remove genotypes for non-phenotyped individuals
-    new_snp2geno = pd.DataFrame()
-    for i in snp2geno:
-        if snp2geno[i].isnull().values.any():
-            continue  # exclude SNPs with null genotypes
-        new_snp2geno[i] = pd.to_numeric(snp2geno[i])
-    return new_snp2geno
-
-
-def get_gene_window(txlist, tx2info, window):
-    chrom = ''  # chromosome this gene is on
-    first_start, last_end = 10**20, 0  # first start post & last end pos among all transcripts
-    for tx in txlist:
-        chrom, start, end, gid, strand = tx2info[tx]
-        if start < first_start:
-            first_start = start
-        if end > last_end:
-            last_end = end
-    # center window around first_start and last_end
-    window_start, window_end = first_start - window, last_end + window
-    return int(window_start), int(window_end), chrom
-
-
-def get_betas_stderrs(snp_genos, tx2expr, txlist):
-    betas, stderrs, residuals = [], [], []
-    for tx in txlist:
-        # regression_results = linregress(tx2expr[tx], snp_genos)
-        regression_results = linregress(snp_genos, tx2expr[tx])
-        beta, stderr = regression_results[0], regression_results[4]
-        resid = tx2expr[tx] - (snp_genos * beta)
-        betas.append(beta)
-        stderrs.append(stderr)
-        residuals.append(resid)
-    return np.array(betas), np.array(stderrs), np.array(residuals)
-
-
-# implement test proposed by Hashimoto & Ohtani, Econometrics Letters (1990)
-def hashimoto_ohtani_t2_stat(betas, residuals, genos):
-    p = M = len(betas)  # number of isoforms
-    # R = np.identity(M)  # linear restrictions on betas
-    T = len(genos)  # sample size
-    K = 1  # num regressors
-    r = int((T-K) / K)
-    dof = r - p + 1
-    X = genos  # (genos - np.mean(genos)) / np.std(genos)
-    Q = np.sqrt(1 / np.dot(X, X))
-    # Q = np.array([1 / np.dot(X, X)])
-    # Q = np.invert(np.matmul(np.transpose(X), X))
-    Sigma = np.zeros((M, M))
-    for i in range(M):
-        for j in range(M):
-            Sigma[i][j] = np.dot(residuals[i], residuals[j]) / (T - K)
-
-    N = np.identity(T) - np.outer(X * (1 / np.dot(X, X)), np.transpose(X))
-    eigvals, eigvecs = np.linalg.eigh(N)
-    eigvals_1 = [i for i in range(len(eigvals)) if 0.999 < eigvals[i] < 1.001]
-    z = np.array([v[eigvals_1] for v in eigvecs])
-    resid_star = np.array([np.matmul(np.transpose(z), residuals[i]) for i in range(len(residuals))])
-
-    S = np.zeros((M, M))
-    for j in range(r):  # T):
-        vec_e_j = np.array([e[j] for e in resid_star])
-        # kron_prod = np.kron(np.identity(M), np.transpose(Q))
-        kron_prod = np.identity(M) * Q
-        eta_j = np.matmul(kron_prod, np.transpose(vec_e_j))
-        S += np.outer(eta_j, np.transpose(eta_j))
-    S /= r
-
-    hotelling_t2_stat = (np.matmul(np.matmul(np.transpose(betas), np.linalg.inv(S)), betas) / r) * ((r - p + 1) / p)
-    ncp = np.matmul(np.matmul(betas, np.linalg.inv(Sigma / np.dot(X, X))), betas)
-    pval = 1 - ncfdtr(p, dof, ncp, hotelling_t2_stat)
-    # print(p, dof, ncp, hotelling_t2_stat, pval)
-    return hotelling_t2_stat, pval
-
-
-def find_peak_snp(tx2expr, snp2geno, txlist, permuted_tx2expr):
-    peak_snp, peak_stat, peak_pval = 'rsid', 0, 1
-    perm_peak_pvals = [1.0 for i in range(len(permuted_tx2expr))]
-    for snp in snp2geno:
-        betas, stderrs, residuals = get_betas_stderrs(snp2geno[snp], tx2expr, txlist)
-        stat, pval = hashimoto_ohtani_t2_stat(betas, residuals, np.array(snp2geno[snp]))
-        if pval < peak_pval:
-            peak_snp = snp
-            peak_stat = stat
-            peak_pval = pval
-        for i in range(len(permuted_tx2expr)):
-            betas, stderrs, residuals = get_betas_stderrs(snp2geno[snp], permuted_tx2expr[i], txlist)
-            stat, pval = hashimoto_ohtani_t2_stat(betas, residuals, np.array(snp2geno[snp]))
-            if pval < perm_peak_pvals[i]:
-                perm_peak_pvals[i] = pval
-    return peak_snp, peak_stat, peak_pval, perm_peak_pvals
-
-
-def set_initial_beta_params(pval_vec):
-    pval_mean = np.mean(pval_vec)
-    pval_var = np.var(pval_vec)
-    beta_shape1 = pval_mean * (pval_mean * (1.0 - pval_mean) / pval_var - 1.0)
-    beta_shape2 = beta_shape1 * (1.0 / pval_mean - 1.0)
-    return beta_shape1, beta_shape2
-
-
-# def beta_likelihood_function(beta_shape1, beta_shape2, pval_vec):
-def beta_likelihood_function(beta_params, other_params):
-    beta_shape1, beta_shape2 = beta_params
-    likelihood_sum1, likelihood_sum2, n_var = other_params
-    beta_func_res = betaln_func(beta_shape1, beta_shape2)
-    return -1.0 * ((beta_shape1 - 1) * likelihood_sum1 + (beta_shape2 - 1) * likelihood_sum2 - n_var * beta_func_res)
-
-
-def compute_beta_perm_pvals(nominal_pval, perm_peak_pvals):
-    beta_shape1, beta_shape2 = set_initial_beta_params(perm_peak_pvals)
-    beta_params = np.array([beta_shape1, beta_shape2])
-    likelihood_sum1 = sum([np.log(i) for i in perm_peak_pvals])
-    likelihood_sum2 = sum([np.log(1 - i) for i in perm_peak_pvals])
-    n_var = len(perm_peak_pvals)
-    other_params = [likelihood_sum1, likelihood_sum2, n_var]
-    res = minimize(beta_likelihood_function, x0=beta_params, args=other_params, method='nelder-mead')
-    beta_shape1, beta_shape2 = res.x
-    adjusted_pval = 1 - beta.sf(nominal_pval, beta_shape1, beta_shape2)
-    return adjusted_pval
-
-
-def permute_transcripts(tx2expr, txlist, n_perms):
-    tx_perms = []
-    for i in range(n_perms):
-        permuted_dict = {}
-        for tx in txlist:
-            permuted_dict[tx] = np.copy(tx2expr[tx])
-            np.random.shuffle(permuted_dict[tx])
-        tx_perms.append(permuted_dict)
-    return np.transpose(np.array(tx_perms))
-
-
-def nominal_pass(vcf, tx2info, tx2expr, gene_to_tx, meta_lines, window, bcftools, n_perms):
-    fnull = open(os.devnull, 'w')  # used to suppress some annoying bcftools warnings
-    gene_results, perm_pvals = {}, {}
-    for gene in gene_to_tx:
-        txlist = gene_to_tx[gene]
-        # get window around gene and subset the vcf for that window using bcftools
-        window_start, window_end, chrom = get_gene_window(txlist, tx2info, window)
-        window_str = str(chrom) + ':' + str(window_start) + '-' + str(window_end)
-        subprocess.Popen([bcftools, 'view', vcf, '-r', window_str, '-o', 'TEMP_isoqtl'], stderr=fnull).wait()
-        # read in SNPs in the window and clear out null SNPs and non-phenotyped individuals
-        gene_window_snps = pd.read_csv('TEMP_isoqtl', sep='\t', header=meta_lines+4, index_col=2).T
-        subprocess.Popen(['rm', 'TEMP_isoqtl']).wait()
-        snp2info, snp2geno = gene_window_snps.iloc[:8], gene_window_snps.iloc[8:]
-        snp2geno = preprocess_snp2geno(tx2expr, snp2geno)
-        # find and store the peak SNP for this gene and its association statistic and p-value
-        if n_perms > 0:
-            permuted_tx2expr = permute_transcripts(tx2expr, txlist, n_perms)
-            snp, fstat, pval, perm_peak_pvals = find_peak_snp(tx2expr, snp2geno, txlist, permuted_tx2expr)
-            dir_perm_pval = (1.0 + sum([pval >= perm_i for perm_i in perm_peak_pvals])) / float(n_perms + 1)
-            if dir_perm_pval < 1.0:
-                beta_perm_pval = compute_beta_perm_pvals(pval, perm_peak_pvals)
-            else:
-                beta_perm_pval = 1.0
-            perm_pvals[gene] = [dir_perm_pval, beta_perm_pval]
-        else:
-            snp, fstat, pval, perm_peak_pvals = find_peak_snp(tx2expr, snp2geno, txlist, {})
-        gene_results[gene] = [snp, fstat, pval]
-    fnull.close()
-    return gene_results, perm_pvals
-
-
 def write_results(results, perm_results, out_fname, nominal_cutoff):
     with(open(out_fname, 'w')) as outfile:
         if len(perm_results) > 0:
@@ -300,5 +136,5 @@ if __name__ == "__main__":
     meta_lines = check_vcf(args.vcf, tx2expr)
     # print('Performing nominal pass...')
     nominal_results, perm_results = nominal_pass(
-        args.vcf, tx2info, tx2expr, gene_to_tx, meta_lines, args.window, args.bcftools, args.permute)
+        args.vcf, tx2info, tx2expr, gene_to_tx, meta_lines, args.window, args.bcftools, args.permute, args.nominal)
     write_results(nominal_results, perm_results, args.output, args.nominal)
---
+++ isoqtl_cis_pass.pyx
@@ -0,0 +1,193 @@
+import os
+import subprocess
+import pandas as pd
+
+from scipy.special import betaln as betaln_func
+from scipy.special import ncfdtr
+from scipy.stats import linregress, beta
+from scipy.optimize import minimize
+
+import numpy as np
+cimport numpy as np
+DTYPE = np.double
+ctypedef np.double_t DTYPE_t
+
+
+def preprocess_snp2geno(tx2expr, snp2geno):
+    # remove genotypes for non-phenotyped individuals and remove "nan" entries
+    snp2geno = (snp2geno.T[list(tx2expr.index)]).T  # remove genotypes for non-phenotyped individuals
+    new_snp2geno = pd.DataFrame()
+    for i in snp2geno:
+        if snp2geno[i].isnull().values.any():
+            continue  # exclude SNPs with null genotypes
+        new_snp2geno[i] = pd.to_numeric(snp2geno[i])
+    return new_snp2geno
+
+
+def get_gene_window(txlist, tx2info, window):
+    chrom = ''  # chromosome this gene is on
+    first_start, last_end = 10**20, 0  # first start post & last end pos among all transcripts
+    for tx in txlist:
+        chrom, start, end, gid, strand = tx2info[tx]
+        if start < first_start:
+            first_start = start
+        if end > last_end:
+            last_end = end
+    # center window around first_start and last_end
+    window_start, window_end = first_start - window, last_end + window
+    return int(window_start), int(window_end), chrom
+
+
+cdef double get_beta(x, double var_x, double mean_x, y, double mean_y):
+    cdef int n = len(x)
+    cdef double cov_xy = 0.0
+    cdef int i
+    for i in range(n):
+        cov_xy += (x[i] - mean_x)*(y[i] - mean_y)
+    cov_xy /= n
+    cdef double beta = cov_xy / var_x
+    return beta
+
+
+def get_betas_stderrs(snp_genos, tx2expr, txlist, genos_var, genos_mean):
+    betas, residuals = [], []
+    for tx in txlist:
+        #regression_results = linregress(snp_genos, tx2expr[tx])
+        #beta, stderr = regression_results[0], regression_results[4]
+        tx_arr = np.array(tx2expr[tx])
+        beta = get_beta(np.array(snp_genos), genos_var, genos_mean, tx_arr, np.mean(tx_arr))
+        resid = tx2expr[tx] - (snp_genos * beta)
+        betas.append(beta)
+        residuals.append(resid)
+    return np.array(betas), np.array(residuals)
+
+
+# implement test proposed by Hashimoto & Ohtani, Econometrics Letters (1990)
+cdef (double, double) hashimoto_ohtani_t2_stat(np.ndarray[DTYPE_t, ndim=1] betas, np.ndarray[DTYPE_t, ndim=2] residuals, np.ndarray[DTYPE_t, ndim=1] genos, np.ndarray[DTYPE_t, ndim=2] z, double genos_dot_inv):
+    cdef int i
+    cdef double p = len(betas) * 1.0  # number of isoforms
+    cdef int T = len(genos)  # sample size
+    cdef double K = 1.0  # num regressors
+    cdef int r = int((T-K) / K)
+    cdef double dof = r - p + 1
+
+    cdef np.ndarray[DTYPE_t, ndim=2] Sigma = np.matmul(residuals, np.transpose(residuals)) / (T - K)
+    cdef np.ndarray[DTYPE_t, ndim=2] resid_star = np.matmul(residuals, z)
+    cdef np.ndarray[DTYPE_t, ndim=2] S = np.matmul(resid_star, np.transpose(resid_star)) * genos_dot_inv / r
+
+    cdef double hotelling_t2_stat = (np.matmul(np.matmul(np.transpose(betas), np.linalg.inv(S)), betas) / r) * ((r - p + 1) / p)
+    ncp = np.matmul(np.matmul(betas, np.linalg.inv(Sigma / np.dot(genos, genos))), betas)
+    cdef double pval = 1 - ncfdtr(p, dof, ncp, hotelling_t2_stat)
+    # print(p, dof, ncp, hotelling_t2_stat, pval)
+    return hotelling_t2_stat, pval
+
+
+def find_peak_snp(tx2expr, snp2geno, txlist, nominal_thresh, permuted_tx2expr):
+    cdef double peak_stat = 0.0
+    cdef double peak_pval = 1.0
+    cdef double pval = 0.0
+    cdef double stat = 0.0
+    cdef np.ndarray[DTYPE_t, ndim=1] betas
+    cdef np.ndarray[DTYPE_t, ndim=1] stderrs
+    cdef np.ndarray[DTYPE_t, ndim=2] residuals
+    cdef np.ndarray[DTYPE_t, ndim=1] genos
+    cdef np.ndarray[DTYPE_t, ndim=1] perm_peak_pvals = np.ones(len(permuted_tx2expr))
+    cdef int i
+    peak_snp = 'none'
+    # peak_snp, peak_stat, peak_pval = 'rsid', 0.0, 1.0
+    # perm_peak_pvals = [1.0 for i in range(len(permuted_tx2expr))]
+    for snp in snp2geno:
+        genos = np.array(snp2geno[snp]).astype(np.double)
+        genos_var = np.var(genos)
+        genos_mean = np.mean(genos)
+        betas, residuals = get_betas_stderrs(snp2geno[snp], tx2expr, txlist, genos_var, genos_mean)
+        genos = np.array(snp2geno[snp]).astype(np.double)
+        genos_dot_inv = 1 / np.dot(genos, genos)
+        N = np.identity(len(genos)) - np.outer(genos * genos_dot_inv, np.transpose(genos))
+        eigvals, eigvecs = np.linalg.eigh(N)
+        z = np.delete(eigvecs, 0, axis=1)
+        #print(betas) ; print(residuals) ; print(genos) ; print()
+        stat, pval = hashimoto_ohtani_t2_stat(betas, residuals, genos, z, genos_dot_inv)
+        if pval < peak_pval:
+            peak_snp = snp
+            peak_stat = stat
+            peak_pval = pval
+        if pval < nominal_thresh:
+            for i in range(len(permuted_tx2expr)):
+                betas, residuals = get_betas_stderrs(genos, permuted_tx2expr[i], txlist, genos_var, genos_mean)
+                stat, pval = hashimoto_ohtani_t2_stat(betas, residuals, genos, z, genos_dot_inv)
+                if pval < perm_peak_pvals[i]:
+                    perm_peak_pvals[i] = pval
+    return peak_snp, peak_stat, peak_pval, perm_peak_pvals
+
+
+cdef (double, double) set_initial_beta_params(pval_vec):
+    pval_mean = np.mean(pval_vec)
+    pval_var = np.var(pval_vec)
+    beta_shape1 = pval_mean * (pval_mean * (1.0 - pval_mean) / pval_var - 1.0)
+    beta_shape2 = beta_shape1 * (1.0 / pval_mean - 1.0)
+    return beta_shape1, beta_shape2
+
+
+# def beta_likelihood_function(beta_shape1, beta_shape2, pval_vec):
+cdef double beta_likelihood_function(beta_params, other_params):
+    beta_shape1, beta_shape2 = beta_params
+    likelihood_sum1, likelihood_sum2, n_var = other_params
+    beta_func_res = betaln_func(beta_shape1, beta_shape2)
+    return -1.0 * ((beta_shape1 - 1) * likelihood_sum1 + (beta_shape2 - 1) * likelihood_sum2 - n_var * beta_func_res)
+
+
+cdef double compute_beta_perm_pvals(nominal_pval, perm_peak_pvals):
+    beta_shape1, beta_shape2 = set_initial_beta_params(perm_peak_pvals)
+    beta_params = np.array([beta_shape1, beta_shape2])
+    likelihood_sum1 = sum([np.log(i) for i in perm_peak_pvals])
+    likelihood_sum2 = sum([np.log(1 - i) for i in perm_peak_pvals])
+    n_var = len(perm_peak_pvals)
+    other_params = [likelihood_sum1, likelihood_sum2, n_var]
+    res = minimize(beta_likelihood_function, x0=beta_params, args=other_params, method='nelder-mead')
+    beta_shape1, beta_shape2 = res.x
+    adjusted_pval = 1 - beta.sf(nominal_pval, beta_shape1, beta_shape2)
+    return adjusted_pval
+
+
+def permute_transcripts(tx2expr, txlist, n_perms):
+    tx_perms = []
+    for i in range(n_perms):
+        permuted_dict = {}
+        for tx in txlist:
+            permuted_dict[tx] = np.copy(tx2expr[tx])
+            np.random.shuffle(permuted_dict[tx])
+        tx_perms.append(permuted_dict)
+    return np.transpose(np.array(tx_perms))
+
+
+def nominal_pass(vcf, tx2info, tx2expr, gene_to_tx, meta_lines, window, bcftools, n_perms, nominal_thresh):
+    fnull = open(os.devnull, 'w')  # used to suppress some annoying bcftools warnings
+    gene_results, perm_pvals = {}, {}
+    for gene in gene_to_tx:
+        txlist = gene_to_tx[gene]
+        # get window around gene and subset the vcf for that window using bcftools
+        window_start, window_end, chrom = get_gene_window(txlist, tx2info, window)
+        window_str = str(chrom) + ':' + str(window_start) + '-' + str(window_end)
+        subprocess.Popen([bcftools, 'view', vcf, '-r', window_str, '-o', 'TEMP_isoqtl'], stderr=fnull).wait()
+        # read in SNPs in the window and clear out null SNPs and non-phenotyped individuals
+        gene_window_snps = pd.read_csv('TEMP_isoqtl', sep='\t', header=meta_lines+4, index_col=2).T
+        subprocess.Popen(['rm', 'TEMP_isoqtl']).wait()
+        snp2info, snp2geno = gene_window_snps.iloc[:8], gene_window_snps.iloc[8:]
+        snp2geno = preprocess_snp2geno(tx2expr, snp2geno)
+        # find and store the peak SNP for this gene and its association statistic and p-value
+        if n_perms > 0:
+            permuted_tx2expr = permute_transcripts(tx2expr, txlist, n_perms)
+            snp, fstat, pval, perm_peak_pvals = find_peak_snp(tx2expr, snp2geno, txlist, nominal_thresh, permuted_tx2expr)
+            if pval < nominal_thresh:
+                dir_perm_pval = (1.0 + sum([pval >= perm_i for perm_i in perm_peak_pvals])) / float(n_perms + 1)
+                if dir_perm_pval < 1.0:
+                    beta_perm_pval = compute_beta_perm_pvals(pval, perm_peak_pvals)
+                else:
+                    beta_perm_pval = 1.0
+                perm_pvals[gene] = [dir_perm_pval, beta_perm_pval]
+        else:
+            snp, fstat, pval, perm_peak_pvals = find_peak_snp(tx2expr, snp2geno, txlist, nominal_thresh, {})
+        gene_results[gene] = [snp, fstat, pval]
+    fnull.close()
+    return gene_results, perm_pvals
